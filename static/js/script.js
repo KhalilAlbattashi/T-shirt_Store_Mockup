533118aad3f20f4bc1f1a99c5c086d4860d335d6
Khalil Albattashi <albattashi.ka@gmail.com>
2023-06-18 20:28:05 +0400

    added close and open functionalties for the menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("Test");
+const exit_btn = document.querySelector(".icon");
+const menu = document.querySelector(".customize_window");
+const customize_btn = document.querySelector(".customize_btn");
+
+customize_btn.addEventListener("click", open);
+exit_btn.addEventListener("click", closeNav);
+
+
+function open(){
+    menu.style.display = "block";
+}
+
+function closeNav() {
+    menu.style.display = "none";
+}
